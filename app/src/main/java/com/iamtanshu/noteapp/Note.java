f52c5039d9587fcf7eb52372c35f774de36b372f
Sheshank Srivastava <ambitious9350164795@gmail.com>
2021-01-14 17:37:25 +0530

    set note entity

---
+++ app/src/main/java/com/iamtanshu/noteapp/Note.java
@@ -0,0 +1,40 @@
+package com.iamtanshu.noteapp;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName ="note_table")
+public class Note {
+    @PrimaryKey(autoGenerate =true)
+    private int id;
+    private String title;
+    private String decription;
+    private int priority;
+
+    public Note(String title, String decription, int priority) {
+        this.title = title;
+        this.decription = decription;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDecription() {
+        return decription;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}

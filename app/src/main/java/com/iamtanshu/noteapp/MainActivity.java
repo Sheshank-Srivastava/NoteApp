1280793948210288daa75f960d90739cebde7fa0
Sheshank Srivastava <ambitious9350164795@gmail.com>
2021-01-14 23:35:02 +0530

    User Functionality remain

--- app/src/main/java/com/iamtanshu/noteapp/MainActivity.java
+++ app/src/main/java/com/iamtanshu/noteapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.iamtanshu.noteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStore;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.iamtanshu.noteapp.NoteDatabase.Note;
+import com.iamtanshu.noteapp.ViewModel.NoteViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        noteViewModel = new NoteViewModel(getApplication());
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "OnChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
